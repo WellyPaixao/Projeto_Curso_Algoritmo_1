b7700ddd2747f5ac592bca9ca80a1f5819640d29
WellyPaixao <ulpaixao10@outlook.com>
2020-07-29 23:33:18 -0300

    Removido os comentários do fonte.

--- Concatenacao.js
+++ Concatenacao.js
@@ -1,21 +1,3 @@
-/*
-Var
-// Seção de Declarações das variáveis 
-   nome: caractere
-   numero: real
-
-Inicio
-// Seção de Comandos, procedimento, funções, operadores, etc... 
-   escreval("Digite um nome")
-   leia(nome)
-   escreval("Digite um numero")
-   leia(numero)
-   
-   escreval(nome, ":" , numero, 15)
-
-Fimalgoritmo
-*/
-
 var nome, numero;
 
 nome = prompt("Digite um nome:");
